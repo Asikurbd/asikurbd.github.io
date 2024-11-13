e279b1c0ea55d76f8ca9113d0f373d030f0c4e4b
Asikur Rahman <asikurbd25@gmail.com>
2024-11-13 10:43:10 +0600

    Delete jss.js

--- jss.js
+++
@@ -1,7 +0,0 @@
-function callAll(jsfiles) {
-    var src = document.createElement("script");
-    src.setAttribute("type", "text/javascript");
-    src.setAttribute("src", jsfiles);
-    document.getElementsByTagName("head")[0].appendChild(src);
-}
-callAll("https://asikurbd.github.io/js/include.js");
